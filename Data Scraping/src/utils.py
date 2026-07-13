066f03dbf69c53bf9f098efb7d15d893203c16fc
ValentinoDan <19624107@mahasiswa.itb.ac.id>
2026-07-13 11:03:44 +0700

    feat: add helper funcs

---
+++ Data Scraping/src/utils.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {"User-Agent": "basdat-scraper-assignment (contact: 13524104@std.stei.itb.ac.id)"}
+
+# fetch url
+def fetch(url):
+    response = requests.get(url, headers=headers)
+    return BeautifulSoup(response.content, "html.parser")
+
+# remove $ dan ubah ke int
+def remove_sign(text):
+    if not text:
+        return None
+    text = text.replace("$", "").replace(",", "").replace("−", "-").strip()
+    try:
+        return int(text)
+    except ValueError:
+        return None
+    
+# remove % dan ubah ke float
+def remove_pct(text):
+    if not text:
+        return None
+    text = text.replace("%", "").replace("−", "-").strip()
+    try:
+        return float(text)
+    except ValueError:
+        return None
+    
+# remove $ dan ubah ke float
+def remove_sign_float(text):
+    if not text:
+        return None
+    text = text.replace(",", "").replace("−", "-").strip()
+    try:
+        return float(text)
+    except ValueError:
+        return None
