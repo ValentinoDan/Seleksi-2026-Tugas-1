83fc7333a4340f93fc9e580a908e862ff1652cd1
ValentinoDan <19624107@mahasiswa.itb.ac.id>
2026-07-17 18:00:05 +0700

    feat: add drop tables and unique attribute

--- Data Storing/src/IndikatorNegara.sql
+++ Data Storing/src/IndikatorNegara.sql
@@ -1,11 +1,22 @@
+DROP TABLE IF EXISTS Komposisi_Energi;
+DROP TABLE IF EXISTS Indikator_Energi;
+DROP TABLE IF EXISTS Indikator_CO2;
+DROP TABLE IF EXISTS Indikator_Populasi;
+DROP TABLE IF EXISTS Indikator_GDP;
+DROP TABLE IF EXISTS Negara;
+DROP TABLE IF EXISTS Jenis_Energi;
+DROP TABLE IF EXISTS Tahun;
+DROP TABLE IF EXISTS Kategori_Ekonomi;
+DROP TABLE IF EXISTS Benua;
+
 CREATE TABLE Benua (
     id_benua SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    nama_benua VARCHAR(100) NOT NULL
+    nama_benua VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE Kategori_Ekonomi (
     id_kategori_ekonomi SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    jenis_kategori VARCHAR(100) NOT NULL
+    jenis_kategori VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE Tahun (
@@ -19,7 +30,7 @@ CREATE TABLE Jenis_Energi (
 
 CREATE TABLE Negara (
     id_negara SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    nama_negara VARCHAR(100) NOT NULL,
+    nama_negara VARCHAR(100) NOT NULL UNIQUE,
     id_benua SMALLINT NOT NULL,
     id_kategori_ekonomi SMALLINT,
     FOREIGN KEY (id_benua) REFERENCES Benua(id_benua),
@@ -30,9 +41,9 @@ CREATE TABLE Indikator_GDP (
     id_negara SMALLINT,
     tahun SMALLINT,
     nilai_gdp BIGINT,
-    persentase_pertumbuhan_gdp DECIMAL(4,2),
+    persentase_pertumbuhan_gdp DECIMAL(5,2),
     gdp_per_capita INT,
-    persentase_gdp_dunia DECIMAL(4, 2),
+    persentase_gdp_dunia DECIMAL(5, 2),
     rank_gdp SMALLINT,
     primary KEY (id_negara, tahun),
     FOREIGN KEY (id_negara) REFERENCES Negara(id_negara),
@@ -43,15 +54,15 @@ CREATE TABLE Indikator_Populasi (
     id_negara SMALLINT,
     tahun SMALLINT,
     jumlah_populasi BIGINT,
-    persentase_perubahan_tahunan DECIMAL(4, 2),
+    persentase_perubahan_tahunan DECIMAL(5, 2),
     perubahan_tahunan INT,
     migrasi_bersih INT,
     usia_median DECIMAL(4, 2),
     tingkat_fertilitas DECIMAL(4, 2),
     kepadatan_penduduk SMALLINT,
-    persentase_penduduk_urban DECIMAL(4, 2),
+    persentase_penduduk_urban DECIMAL(5, 2),
     jumlah_penduduk_urban INT,
-    persentase_populasi_dunia DECIMAL(4, 2),
+    persentase_populasi_dunia DECIMAL(5, 2),
     rank_populasi SMALLINT,
     PRIMARY KEY (id_negara, tahun),
     FOREIGN KEY (id_negara) REFERENCES Negara(id_negara),
@@ -62,9 +73,9 @@ CREATE TABLE Indikator_CO2 (
     id_negara SMALLINT,
     tahun SMALLINT,
     emisi_co2 BIGINT,
-    persentase_perubahan_setahun DECIMAL(4, 2),
-    emisi_co2_per_kapita DECIMAL(4, 2),
-    persentase_emisi_co2_dunia DECIMAL(4, 2),
+    persentase_perubahan_setahun DECIMAL(5, 2),
+    emisi_co2_per_kapita DECIMAL(5, 2),
+    persentase_emisi_co2_dunia DECIMAL(5, 2),
     rank_co2 SMALLINT,
     PRIMARY KEY (id_negara, tahun),
     FOREIGN KEY (id_negara) REFERENCES Negara(id_negara),
@@ -75,7 +86,7 @@ CREATE TABLE Indikator_Energi (
     id_negara SMALLINT,
     tahun SMALLINT,
     konsumsi_energi_total BIGINT,
-    persentase_dunia DECIMAL(4, 2),
+    persentase_dunia DECIMAL(5, 2),
     konsumsi_per_capita INT,
     rank_energi SMALLINT,
     PRIMARY KEY (id_negara, tahun),
